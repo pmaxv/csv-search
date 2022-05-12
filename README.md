4d5bc0697ef4c14e342e2e785742dfc986496189
Max Pogribnyak <bermuda28@yahoo.com>
2022-05-12 18:18:45 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+The script for searching data in CSV file
+
+The script receives as input the path to a CSV file to be imported, a 
+column number in which to search, and a search key.
+
+The script must be invoked in this way:
+
+$ php search.php ./file.csv 2 Alberto
+
+where ./file.csv is the path to a CSV file formatted in this way:
+1,Rossi,Fabio,01/06/1990;
+2,Gialli,Alessandro,02/07/1989;
+3,Verdi,Alberto,03/08/1987;
+
+The number 2 represents the index of the column to search in (in the 
+previous file the name) and Alberto represents the search key.
+
+### Requirements
+* PHP >= 5.6.x
---
+++ search.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @param $data
+ * @return string
+ */
+function find(Array $data)
+{
+    if (empty($data[1])) {
+        return "Please specify the path to the file as the first parameter\r\n";
+    }
+    if (!file_exists($data[1])) {
+        return printf("File '%s' does not exist\r\n", $data[1]);
+    }
+    if ($file = fopen($data[1], 'rb')) {
+        if ('text/csv' !== mime_content_type( $file )) {
+            return "Please provide correct CSV file\r\n";
+        }
+        if (!isset($data[2])) {
+            return "Second argument is required\r\n";
+        }
+        if ($data[2] < 0 || $data[2] > 3) {
+            return "Second argument must be between 0 and 3\r\n";
+        }
+        if (empty($data[3])) {
+            return "Third argument is required\r\n";
+        }
+        if (!is_numeric($data[2])) {
+            return "Second argument has to be an integer\r\n";
+        }
+
+        $result = "Nothing found\r\n";
+        while ($line = fgetcsv($file))
+        {
+            if (!empty($line[$data[2]]) && strtolower($line[$data[2]]) === strtolower($data[3])) {
+                $result = implode(',', $line) . ";\r\n";
+            }
+        }
+        fclose($file);
+        return $result;
+    }
+    return "File is invalid\r\n";
+}
+
+function is_cli()
+{
+    if (defined('STDIN')) {
+        return true;
+    }
+    if (empty($_SERVER['REMOTE_ADDR']) && !isset($_SERVER['HTTP_USER_AGENT']) && count($_SERVER['argv']) > 0) {
+        return true;
+    }
+    return false;
+}
+
+if (is_cli()) {
+    echo find($argv);
+}
